dee5fb0f93ef0f1e8b35b9d9a110184d768d0c48
louischen1997 <47822448+louischen1997@users.noreply.github.com>
2019-05-21 21:04:04 +0800

    dia

---
+++ midterm/src/components/Post/diary/day1.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+
+export default class Day1 extends Component {
+    
+    
+    render() {
+       
+        return (
+            <div>
+                <h1>midterm exam in next week:</h1>
+                <h6>Signal systems</h6>
+                <h6>Algorithm</h6>
+                <h6>Data structure</h6>
+                <h6>Fundamentals of Acoustics</h6>
+                <h6>French</h6>
+                <h6>Logic</h6>
+           </div>
+        );
+    }
+}
---
+++ midterm/src/components/Post/diary/day2.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+
+export default class Day1 extends Component {
+    
+    
+    render() {
+       
+        return (
+            <div>
+            <h1><input placeholder="something to write down"></input></h1>
+           </div>
+        );
+    }
+}
